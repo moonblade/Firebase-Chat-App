9682116a9404f6cc604634aa25a3f60a9468d6b6
moonblade <moonblade168@gmail.com>
2020-02-07 22:25:35 +0530

    add routing

--- public/chat/src/app/app-routing.module.ts
+++ public/chat/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  'path': 'chat',
+  'component': ChatWindowComponent
+}, {
+  path: '',
+  redirectTo: '/chat',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- public/chat/src/app/app.module.ts
+++ public/chat/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule,
