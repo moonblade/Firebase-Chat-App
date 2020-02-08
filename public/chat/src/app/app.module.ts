71dd1af62e0e0ea4f06b759db21c2b3525a4a377
moonblade <moonblade168@gmail.com>
2020-02-08 20:33:01 +0530

    navbar

--- public/chat/src/app/app.module.ts
+++ public/chat/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatWindowComponent
+    ChatWindowComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
